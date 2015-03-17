b2b90231121e3fc3c0844dfcbcdac42036efa82e
SensitiveLion <andypassidomo@gmail.com>
2015-03-17 15:10:26 -0400

    basic website working

--- server.rb
+++ server.rb
@@ -1,13 +1,24 @@
 require "sinatra"
 require "pry"
 
+require_relative "common_tests"
+require_relative "harder_tests"
+require_relative "common_passwords"
+require_relative "testing"
+require_relative "password_summary"
+
+use Rack::Session::Cookie
+
+@check_password = nil
+
 get "/" do
   erb :page
-
 end
 
 post "/" do
-  @password = PasswordSummary.new(params["password"])
+  @test = params["password"]
+  @check_password = PasswordSummary.new(@test)
+  session[:check] = @check_password
 
   redirect "/"
 end
